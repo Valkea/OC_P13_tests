e761f8e6f75422690eb147fb9d6c767d77d91e13
Emmanuel <github-sub@shedge.com>
2021-03-16 23:35:40 +0100

    feat: add 'oc_lettings_site' test

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,11 @@
-def test_dummy():
-    assert 1
+from django.urls import reverse
+from django.test import TestCase
+
+
+class OcLettingsSiteTests(TestCase):
+    def test_happy_index(self):
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertContains(
+            response, "<h1>Welcome to Holiday Homes</h1>", status_code=200
+        )
