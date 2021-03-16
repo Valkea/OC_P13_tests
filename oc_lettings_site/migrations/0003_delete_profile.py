764a121a39e631d85ca99f2c4ebd9ba2eafc6e67
Emmanuel <github-sub@shedge.com>
2021-03-16 16:14:58 +0100

    refactor: black formatting

--- oc_lettings_site/migrations/0003_delete_profile.py
+++ oc_lettings_site/migrations/0003_delete_profile.py
@@ -6,11 +6,11 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('oc_lettings_site', '0002_auto_20210316_1329'),
+        ("oc_lettings_site", "0002_auto_20210316_1329"),
     ]
 
     operations = [
         migrations.DeleteModel(
-            name='Profile',
+            name="Profile",
         ),
     ]
