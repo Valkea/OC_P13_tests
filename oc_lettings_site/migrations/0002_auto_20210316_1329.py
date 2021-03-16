8765d21ad66425e6b8b8a832df1c999b81223e24
Emmanuel <github-sub@shedge.com>
2021-03-16 15:27:24 +0100

    refactor: black formatting 'lettings' app

--- oc_lettings_site/migrations/0002_auto_20210316_1329.py
+++ oc_lettings_site/migrations/0002_auto_20210316_1329.py
@@ -6,18 +6,18 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('oc_lettings_site', '0001_initial'),
+        ("oc_lettings_site", "0001_initial"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='letting',
-            name='address',
+            model_name="letting",
+            name="address",
         ),
         migrations.DeleteModel(
-            name='Address',
+            name="Address",
         ),
         migrations.DeleteModel(
-            name='Letting',
+            name="Letting",
         ),
     ]
