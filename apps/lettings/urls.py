962e0079d67c1750b0d1f9349530b9a6b356cebe
Emmanuel <github-sub@shedge.com>
2021-03-19 22:46:15 +0100

    special: add an extra branch with a minor change in order to test clientCI triggerings

--- apps/lettings/urls.py
+++ apps/lettings/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("lettings/<int:letting_id>/", views.letting, name="letting"),
 
     path('sentry-debug/', views.trigger_error, name="sentry-debug"),
+    path('extra-branch-test/', views.trigger_error, name="sentry-debug"),
 ]
